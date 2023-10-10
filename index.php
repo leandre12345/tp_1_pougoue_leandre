9e62f0e34826035b2265b324dcfa45f60f2aa05c
leandre12345 <denaudpougoue@gmail.com>
2023-10-10 12:57:18 -0400

    init

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 function validerMotDePasse($motDePasse) {
-    // Vérifier la longueur du mot de passe
     if (strlen($motDePasse) < 6 || strlen($motDePasse) > 10) {
         return "Erreur : Le mot de passe doit avoir entre 6 et 10 caractères.";
     }
-
-    // Définir le salt
-    $salt = "ABC1234@";
-
-    // Concaténer le salt au mot de passe
-    $motDePasseConcatene = $motDePasse . $salt;
+    $salt = "LEA10#";
+    $motDePasseAdd = $motDePasse . $salt;
 
     // Chiffrer le mot de passe
-    $motDePasseChiffre = password_hash($motDePasseConcatene, PASSWORD_DEFAULT);
+    $motDePasseEncryptage = password_hash($motDePasseAdd, PASSWORD_DEFAULT);
 
     // Retourner le résultat avec le salt et le mot de passe chiffré
     return [
-        'message' => "Salt : $salt, Mot de passe chiffré : $motDePasseChiffre",
+        'message' => "Salt : $salt, Mot de passe chiffré : $motDePasseEncryptage",
         'salt' => $salt,
-        'motDePasseChiffre' => $motDePasseChiffre
+        'motDePasseEncryptage' => $motDePasseEncryptage
     ];
 }
 ?>
